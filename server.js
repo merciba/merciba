db33c8beba4f56e65cf3f141f71173850d23652f
alfonsogoberjr <alfonso.gober.jr@gmail.com>
2017-01-22 19:34:23 -0500

    fixed compression

--- server.js
+++ server.js
@@ -20,9 +20,6 @@ app.start({
   public: __dirname + '/public',
   middleware: {
     '/*': compress({
-      filter: function (content_type) {
-        return /text/i.test(content_type)
-      },
       threshold: 2048,
       flush: require('zlib').Z_SYNC_FLUSH
     }),
