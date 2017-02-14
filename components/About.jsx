df677d94813393ca6000d5222242f4b818457740
Alfonso Gober <alfonso.gober.jr@gmail.com>
2017-02-13 21:08:17 -0500

    fix About page

--- components/About.jsx
+++ components/About.jsx
@@ -15,7 +15,7 @@ class About extends React.Component {
       }
       else {
         window.addEventListener('scroll', this.handleScroll.bind(this));
-        $('#app').height($(window).height() * 3.25)
+        $('#app').height($(window).height() * 3.5)
         this.handleScroll()
       }
     }
